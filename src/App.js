9f879c7d73d5b1ca522dd59d10110a947115875a
aleksandarstojcevski <aleksandar.stojcevski@inellipse.com>
2020-07-30 23:45:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p> 
+        <p>
+          A lot of changes
         </p>
         <a
           className="App-link"
